8ca2663c82d8909bbcedd1f742415f181655bbc6
ivanyuyifan <ivannnyesss@gmail.com>
2024-11-16 17:19:26 +0800

    add new path

--- 爬虫实践/爬取网站图片.py
+++ 爬虫实践/爬取网站图片.py
@@ -1,8 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+
+output_folder = '/Users/fafaya/Desktop/bubbles/爬虫实践/网站爬取的图片'
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 url = 'https://nocturne-spider.baicizhan.com/practise/31.html'
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'}
+
 response = requests.get(url, headers=headers)
 response.encoding = response.apparent_encoding
 html = response.text
@@ -22,15 +28,17 @@ for content in content_all:
     #下一步是获取高清图网页页面的所有内容
     htmlA = imgResponse.text
     soupA = BeautifulSoup(htmlA, 'lxml')
-    imgResponseA = soupA.find('img').attrs['src']#这里获取的是高清图的source
+    imgResponseA = soupA.find(style="visibility: visible;width: 100%;").attrs['src']#这里获取的是高清图的source
     imgResponseB = requests.get(imgResponseA)
     img = imgResponseB.content
     
     if imgName != '爱上紫禁城':
-        with open(f'{imgName}.jpg', 'wb') as f:
-            f.write(img)
+        img_path = os.path.join(output_folder, f'{imgName}.jpg')
     else:
         count += 1
-        with open(f'{imgName}-{count}.jpg', 'wb') as f:
-            f.write(img)
+        img_path = os.path.join(output_folder, f'{imgName}-{count}.jpg')
+
+    with open(img_path, 'wb') as f:
+        f.write(img)
+    print(f"图片已保存: {img_path}")
         
